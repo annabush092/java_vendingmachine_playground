59d3afb8449543b147d63448ea7aaaf204a3a53e
Anna Bush <annabush092@gmail.com>
2021-05-10 10:36:29 -0400

    first commit

--- src/main/java/VendingMachine.java
+++ src/main/java/VendingMachine.java
@@ -1,5 +1,12 @@
+import java.util.*;
+
 public class VendingMachine {
     boolean on;
+    ArrayList<String> items;
+
+    public VendingMachine() {
+        items = new ArrayList<String>();
+    }
 
     public void powerButton() {
         on = !on;
@@ -7,6 +14,22 @@ public class VendingMachine {
 
     public boolean isOn() {
         return on;
+    }
+
+    public int numItems() {
+        return items.size();
+    }
+
+    public void addItems(ArrayList<String> itemsToAdd) {
+        items.addAll(itemsToAdd);
+    }
+
+    public void removeItemAt(int i) {
+        items.remove(i);
+    }
+
+    public boolean itemsInclude(String item) {
+        return items.contains(item);
     }
 
 }
--- src/test/java/VendingMachineTest.java
+++ src/test/java/VendingMachineTest.java
@@ -1,4 +1,10 @@
+import java.io.*;
+import java.util.*;
+
 import org.junit.*;
+
+import jdk.jfr.Timestamp;
+
 import static org.junit.Assert.*;
 
 public class VendingMachineTest {
@@ -22,4 +28,37 @@ public class VendingMachineTest {
         assertFalse(vm.isOn());
     }
 
+    @Test
+    public void vmCanAddItems() {
+        add3Items();
+
+        assertTrue(vm.numItems() == 3);
+
+        ArrayList<String> secondAddition = new ArrayList<String>();
+        secondAddition.add("chocolate");
+
+        vm.addItems(secondAddition);
+
+        assertTrue(vm.numItems() == 4);
+    }
+
+    @Test
+    public void vmCanRemoveItems() throws IOException {
+
+        add3Items();
+
+        vm.removeItemAt(1);
+
+        assertTrue(vm.numItems() == 2);
+        assertFalse(vm.itemsInclude("granola bar"));
+    }
+
+    private void add3Items() {
+        ArrayList<String> newItems = new ArrayList<String>();
+        newItems.add("candy");
+        newItems.add("granola bar");
+        newItems.add("peanuts");
+
+        vm.addItems(newItems);
+    }
 }
